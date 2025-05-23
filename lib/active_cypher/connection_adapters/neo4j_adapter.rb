0a78b33914fe3979add77ab610ff615f0bd816c2
Abdelkader Boudih <terminale@gmail.com>
2025-05-23 01:09:16 +0100

    fix: extract operation to adapter

--- lib/active_cypher/connection_adapters/neo4j_adapter.rb
+++ lib/active_cypher/connection_adapters/neo4j_adapter.rb
@@ -100,6 +100,97 @@ module ActiveCypher
         metadata.compact
       end
 
+      module Persistence
+        module_function
+
+        def create_record(model)
+          props = model.send(:attributes_for_persistence)
+          labels = if model.class.respond_to?(:labels)
+                     model.class.labels
+                   else
+                     [model.class.label_name.to_s]
+                   end
+          label_string = labels.map { |l| ":#{l}" }.join
+          props_str = props.map do |k, v|
+            value_str = if v.nil?
+                          'NULL'
+                        elsif v.is_a?(String)
+                          "'#{v.gsub("'", "\\\\'")}'"
+                        elsif v.is_a?(Numeric) || v.is_a?(TrueClass) || v.is_a?(FalseClass)
+                          v.to_s
+                        else
+                          "'#{v.to_s.gsub("'", "\\\\'")}'"
+                        end
+            "#{k}: #{value_str}"
+          end.join(', ')
+
+          cypher = "CREATE (n#{label_string} {#{props_str}}) RETURN id(n) AS internal_id"
+          data = model.connection.execute_cypher(cypher, {}, 'Create')
+
+          return false if data.blank? || !data.first.key?(:internal_id)
+
+          model.internal_id = data.first[:internal_id]
+          model.instance_variable_set(:@new_record, false)
+          model.send(:changes_applied)
+          true
+        end
+
+        def update_record(model)
+          changes = model.send(:changes_to_save)
+          return true if changes.empty?
+
+          labels = if model.class.respond_to?(:labels)
+                     model.class.labels
+                   else
+                     [model.class.label_name]
+                   end
+
+          label_string = labels.map { |l| ":#{l}" }.join
+          set_clauses = changes.map do |property, value|
+            if value.nil?
+              "n.#{property} = NULL"
+            elsif value.is_a?(String)
+              "n.#{property} = '#{value.gsub("'", "\\\\'")}'"
+            elsif value.is_a?(Numeric) || value.is_a?(TrueClass) || value.is_a?(FalseClass)
+              "n.#{property} = #{value}"
+            else
+              "n.#{property} = '#{value.to_s.gsub("'", "\\\\'")}'"
+            end
+          end.join(', ')
+
+          cypher = "MATCH (n#{label_string}) WHERE id(n) = #{model.internal_id} SET #{set_clauses} RETURN n"
+          model.connection.execute_cypher(cypher, {}, 'Update')
+
+          model.send(:changes_applied)
+          true
+        end
+
+        def destroy_record(model)
+          labels = if model.class.respond_to?(:labels)
+                     model.class.labels
+                   else
+                     [model.class.label_name]
+                   end
+          label_string = labels.map { |l| ":#{l}" }.join
+
+          cypher = <<~CYPHER
+            MATCH (n#{label_string})
+            WHERE id(n) = #{model.internal_id}
+            DETACH DELETE n
+            RETURN count(*) AS deleted
+          CYPHER
+
+          result = model.connection.execute_cypher(cypher, {}, 'Destroy')
+          if result.present? && result.first[:deleted].to_i.positive?
+            model.instance_variable_set(:@destroyed, true)
+            model.freeze
+            true
+          else
+            false
+          end
+        end
+      end
+
       protected
 
       def protocol_handler_class = ProtocolHandler
